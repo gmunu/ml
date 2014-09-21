538df80524984c6d457fe04a98f78cab7e4c4329
gmunu <oren@gmunu.org>
2014-09-20 21:31:40 -0700

    TestUnivariateLinearRegression

--- tests/ml_tests.py
+++ tests/ml_tests.py
@@ -1,9 +1,40 @@
+from nose import with_setup
+from nose.tools import assert_equal, assert_almost_equal
+from numpy.testing import assert_array_almost_equal
+import numpy as npy
 from ml.ml import *
 
 filename_univariate_linear_data = 'tests/ex1data1.txt'
 filename_multivariate_linear_data = 'tests/ex1data2.txt'
 filename_logistic_data = 'tests/ex2data1.txt'
 filename_neural_network = 'tests/ex4data1.mat'
+
+class TestUnivariateLinearRegression:
+
+    def setup(self):
+        self.lr = LinearRegression(alpha=0.01, max_iters=1500)
+        self.lr.load(filename_univariate_linear_data)
+
+    def teardown(self):
+        self.lr = None
+
+    def test_univariate_linear_regression_cost(self):
+        theta, J_history = self.lr.learn()
+        actual = J_history[0,0]
+        expected = 32.07273
+        assert_almost_equal(actual, expected, places=4)
+
+    def test_univariate_linear_regression_gradient_descent(self):
+        theta, J_history = self.lr.learn()
+        # final cost:
+        J_min = J_history[-1, 0]
+        desired_J_min = 4.483388
+        assert_almost_equal(J_min, desired_J_min, places=5)
+        # final thetas:
+        desired_theta = npy.mat("-3.63029; 1.16636")
+        assert_array_almost_equal(theta, desired_theta, decimal=5)
+
+#@with_setup(setup_univariate_linear_regression, teardown_univariate_linear_regression)
 
 def test_ex1():
     print "Univariate Linear Regression:"
